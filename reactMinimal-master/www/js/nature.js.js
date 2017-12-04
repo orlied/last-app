0554c2215eafc7b58c9b8e80f1a1ac3f975cd031
orlied <orl130l1va@gmail.com>
2017-12-04 20:54:37 +0800

    last-browse-app

---
+++ reactMinimal-master/www/js/nature.js.js
@@ -0,0 +1,22 @@
+var _root = document.getElementById('apple');
+var dom = 
+	
+   
+      <img src='apple.png'>
+    
+ReactDOM.render(dom,_root); 
+
+var _root = document.getElementById('Banana');
+var dom = 
+	
+   
+      <img src='Banana.jpg'>
+ReactDOM.render(dom,_root); 
+
+var _root = document.getElementById('CHERRY');
+var dom = 
+	
+   
+      <img src="img/CHERRY.jpg">
+    
+ReactDOM.render(dom,_root); 
---
+++ reactMinimal-master/www/js/process.js
@@ -0,0 +1,299 @@
+// <video width="320" height="240" controls>
+//   <source src="movie.mp4" type="video/mp4">
+//   <source src="movie.ogg" type="video/ogg">
+//   Your browser does not support the video tag.
+// </video>
+
+ 
+
+  var nature =
+
+<div>
+
+      <div className="swiper-slide"><img src="img/image.jpg" />
+        <div className="parent_div">
+          <button className="button">A-Beautiful-Path-Villaviciosa-Asturias-Spain
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image2.jpg" />
+        <div className="parent_div">
+          <button className="button">Hotels the rich and famous St. Mary Jamaica
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image3.jpg" />
+        <div className="parent_div">
+          <button className="button">Lower Lewis River Falls, Glifford Pinchot National forest
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image4.jpg" />
+        <div className="parent_div">
+          <button className="button">Migrating animals, Serengeti park- country - Tanzania
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image5.jpg" />
+        <div className="parent_div">
+          <button className="button">Milford Sound  New Zealand
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image6.jpg" />
+        <div className="parent_div">
+          <button className="button">Normandy  Plage
+		  </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image7.jpg" />
+        <div className="parent_div">
+          <button className="button">Oman Country
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image8.jpg" />
+        <div className="parent_div">
+          <button className="button">Most beautiful road, Northern Ireland
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image9.jpg" />
+        <div className="parent_div">
+          <button className="button">Pagan, the thousand pagodas plain, Myanmar
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image10.jpg" />
+        <div className="parent_div">
+          <button className="button">Paracel Islands,Vietnam
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image11.jpg" />
+        <div className="parent_div">
+          <button className="button">Phong Nha Cave Quang Binh Province, Vietnam
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image12.jpg" />
+        <div className="parent_div">
+          <button className="button">PonGua waterfalls, DA LAT, Vietnam
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image13.jpg" />
+        <div className="parent_div">
+          <button className="button">Porto katsiki beach, lefkada island, Greece
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image14.jpg" />
+        <div className="parent_div">
+          <button className="button">Potaro River, Central Guyana
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image15.jpg" />
+        <div className="parent_div">
+          <button className="button">Rice fields, An Giang Province Vietnam
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image16.jpg" />
+        <div className="parent_div">
+          <button className="button">Seychelles - The Pearl, Indian Ocean
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image17.jpg" />
+        <div className="parent_div">
+          <button className="button">Spirit Island on Maligne lake, Jasper National Park
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image18.jpg" />
+        <div className="parent_div">
+          <button className="button">spratly island, Vietnam
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image19.jpg" />
+        <div className="parent_div">
+          <button className="button">The Abu Simbel temple- country - Egypt
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image20.jpg" />
+        <div className="parent_div">
+          <button className="button">The Bora-Bora island- country - French Polynesia'
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image21.jpg" />
+        <div className="parent_div">
+          <button className="button">Angkor City country - Cambodia
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image22.jpg" />
+        <div className="parent_div">
+          <button className="button">The great coral reef- country - Australia
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image23.jpg" />
+        <div className="parent_div">
+          <button className="button">The Iguazu waterfalls-country  Argentina, Brazil
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image24.jpg" />
+        <div className="parent_div">
+          <button className="button">The Niagara waterfalls- country  Canada - United States of America
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image25.jpg" />
+        <div className="parent_div">
+          <button className="button">The pyramids and the sphinx-country, Egypt
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image26.jpg" />
+        <div className="parent_div">
+          <button className="button">The salar Uyuni country, Bolivia
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image27.jpg" />
+        <div className="parent_div">
+          <button className="button">The Tadj Mahall ountry, India
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image28.jpg" />
+        <div className="parent_div">
+          <button className="button">The Victoria waterfalls Country  Zimbabwe, Zambia
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image29.jpg" />
+        <div className="parent_div">
+          <button className="button">Tulip National Farm, Netherlands
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image30.jpg" />
+        <div className="parent_div">
+          <button className="button">Vernazza Rocky Village, Italy
+          </button>
+        </div>
+      </div> 
+      <div className="swiper-slide"><img src="img/image31.jpg" />
+        <div className="parent_div">
+          <button className="button">Viceroy Bali Hotel Bali, INDONESIA
+          </button>
+        </div>
+      </div>
+      <div className="swiper-slide"><img src="img/image32.jpg" />
+        <div className="parent_div">
+          <button className="button">Vinhhalong
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image33.jpg" />
+        <div className="parent_div">
+          <button className="button">XUan Huong Lake - Lam Dong Province, Vietnam
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image34.jpg" />
+        <div className="parent_div">
+          <button className="button">Yuanyang waterfall, China
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image35.jpg" />
+        <div className="parent_div">
+          <button className="button">Zhangjiajie, China
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image36.jpg" />
+        <div className="parent_div">
+          <button className="button">Zhangjiajie National Forest Park Pandora on Earth China
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image37.jpg" />
+        <div className="parent_div">
+          <button className="button">Beach, Thailand
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image38.jpg" />
+        <div className="parent_div">
+          <button className="button">Blue lagoon Grindavik, Iceland
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image39.jpg" />
+        <div className="parent_div">
+          <button className="button">Buddha at Ngong Ping, Lantau Island, Hongkong
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image40.jpg" />
+        <div className="parent_div">
+          <button className="button">Avshalom Cave, ISrael
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image41.jpg" />
+        <div className="parent_div">
+          <button className="button">Fernando De Noronha, Brazil
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image42.jpg" />
+        <div className="parent_div">
+          <button className="button">Awaawapuhi Trail, kauai, Hawaii
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image43.jpg" />
+        <div className="parent_div">
+          <button className="button">Green-Valley-Peak-District, England
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image44.jpg" />
+        <div className="parent_div">
+          <button className="button">Gudvangen-Fjordtell, Norway
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image45.jpg" />
+        <div className="parent_div">
+          <button className="button">Bãi biển Golden Horn, Croatia
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image46.jpg" />
+        <div className="parent_div">
+          <button className="button">Cộng hòa, Maldives
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image47.jpg" />
+        <div className="parent_div">
+          <button className="button">Hotaru Ika Firefly squids, Japan
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image48.jpg" />
+        <div className="parent_div">
+          <button className="button">A Blue Universe, Japan
+          </button></div>  
+      </div>
+	   <div className="swiper-slide"><img src="img/image49.jpg" />
+        <div className="parent_div">
+          <button className="button">A temple On take Volcanic eruptio, Japan
+          </button></div>  
+      </div>
+	  <div className="swiper-slide"><img src="img/image50.jpg" />
+        <div className="parent_div">
+          <button className="button">Yunnan, China
+          </button></div>  
+      </div>
+	  
+ 
+    </div>
+ 
+  
+ReactDOM.render(nature,document.getElementById('root'));
